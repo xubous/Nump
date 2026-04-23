4fd58b97d601853f9ad0497724397eaf1b543d0a
Gabriel Carvalho <gcs232005@gmail.com>
2026-04-23 16:31:56 -0300

    ...

--- src/main/java/project/xubous/Nump/controller/FileController.java
+++ src/main/java/project/xubous/Nump/controller/FileController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
